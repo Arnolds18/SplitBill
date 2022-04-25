00736dce518ae24ac09fe4e68429d571157fd6c3
Arnold Sidiprasetija <arnolds1809@gmail.com>
2022-04-25 15:22:38 +0700

    Initial Commit

---
+++ SplitBill/SplitBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitBillApp.swift
+//  SplitBill
+//
+//  Created by Arnold Sidiprasetija on 25/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SplitBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
